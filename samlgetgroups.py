fd5231eb82e08862544b6a556f494010851d0e02
deku-m <37069737+deku-m@users.noreply.github.com>
2023-06-29 07:38:24 +0200

    Update samlgetgroups.py

--- samlgetgroups.py
+++ samlgetgroups.py
@@ -1,1 +1,34 @@
+from django.contrib.auth.models import Group
+
+class AuthFailed(Exception):
+        pass
+
+def set_role(response, user, backend, *args, **kwargs):
+    try:
+        conndetails = user.social_auth.get(provider='saml')
+        roles = conndetails.extra_data['groups']
+    except KeyError:
+        user.groups.clear()
+        raise AuthFailed("No role assigned")
+
+    try:
+        user.is_superuser = False
+        user.is_staff = False
+
+        for role in roles:
+            if role == 'Group Network Admins':
+                user.is_superuser = True
+                user.save()
+                user.is_staff = True
+                user.save()
+            else:
+                user.is_superuser = False
+                user.save()
+                user.is_staff = False
+                user.save()
+
+        group, created = Group.objects.get_or_create(name=role)
+        group.user_set.add(user)
+    except Group.DoesNotExist:
+        pass
 
