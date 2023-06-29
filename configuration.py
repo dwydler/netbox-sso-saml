be0acd637dbd93d5e768bfafede40283761d572e
deku-m <37069737+deku-m@users.noreply.github.com>
2023-06-29 07:38:00 +0200

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,1 +1,40 @@
+# Remote authentication support
+REMOTE_AUTH_ENABLED = True
+REMOTE_AUTH_BACKEND = 'social_core.backends.saml.SAMLAuth'
+SOCIAL_AUTH_SAML_SP_ENTITY_ID = 'https://netbox.example.local'
+SOCIAL_AUTH_SAML_SP_PUBLIC_CERT = 'binary-certificate-netbox-see.crt-file'
+SOCIAL_AUTH_SAML_SP_PRIVATE_KEY = 'binary-key-certificate-netbox-see-.key-file'
+SOCIAL_AUTH_SAML_ORG_INFO = {
+                "en-US": {
+               "name": "Netbox",
+                "displayname": "Netbox",
+                "url": "https://netbox.example.local"
+        }
+}
+SOCIAL_AUTH_SAML_TECHNICAL_CONTACT = {
+                "emailAddress": "email@emailaddress.com",
+                "givenName": "Technical"
+                }
+
+SOCIAL_AUTH_SAML_SUPPORT_CONTACT = {
+                "emailAddress": "email@emailaddress.com",
+                "givenName": "Support"
+                }
+SOCIAL_AUTH_SAML_ENABLED_IDPS = {
+                "SAML": {
+                "entity_id": "https://sts.windows.net/id-app-on-azure/",
+                "url": "https://login.microsoftonline.com/id-app-on-azure/saml2",
+                "attr_user_permanent_id": "name_id",
+                "attr_username": "name_id",
+                "attr_first_name": "attr_first_name",
+                "attr_last_name": "attr_last_name",
+                "attr_email": "attr_email",
+                "attr_full_name": "attr_full_name",
+                "x509cert": "app-certificate-on-azure-"
+                }
+}
+REMOTE_AUTH_AUTO_CREATE_USER = True
+SOCIAL_AUTH_SAML_EXTRA_DATA = [("http://schemas.microsoft.com/ws/2008/06/identity/claims/groups", "groups")]
+# SOCIAL_AUTH_SAML_SECURITY_CONFIG = {"requestedAuthnContext": False}
+# SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
 
